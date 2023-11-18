4349a2aba75099690ec92a2b9a868fcb1bb6fcc7
TwoLobster <rahulkaundal31000@gmail.com>
2023-11-18 10:48:53 +0530

    Centralising changes to github

---
+++ calc.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int a=12,b=23;
+    printf("a=%d", a);
+    printf("b=%d", b);
+    printf("After subtraction: a-b=%d", a-b);
+    printf("after multiplication: a*b=%d", a*b);
+    printf("after division: a/b=%d", a/b);
+}
